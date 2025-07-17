59b5ed59a8a295a7eca6a3638d5f2333497d312e
Lemonaidd <lemon.cookier@gmail.com>
2025-07-17 00:13:32 -0600

    Add files via upload

--- lolpull.py
+++ lolpull.py
@@ -23,13 +23,11 @@ def display_menu():
 ██║     ╚██████╔╝███████╗███████╗
 ╚═╝      ╚═════╝ ╚══════╝╚══════╝
     """)
-    print("\033[1;32m LOL Social Pull Tool!\033[0m")
-    print("\033[1;34m1. Discord IP Puller\033[0m")
-    print("\033[1;34m2. Xbox IP Puller\033[0m")
-    print("\033[1;34m3. Playstation IP Puller\033[0m")
-    print("\033[1;34m4. InstaGram IP Puller\033[0m")
-    print("\033[1;34m5. TikTok IP Puller\033[0m")
-    print("\033[1;34m6. Snapchat IP Puller\033[0m")
+    print("\033[1;32m LOL Social IP Lookup Tool!\033[0m")
+    print("\033[1;34m1. Xbox IP Lookup\033[0m")
+    print("\033[1;34m2. Playstation IP Lookup\033[0m")
+    print("\033[1;34m3. Skype IP Lookup\033[0m")
+    print("\033[1;34m3. TikTok IP Lookup\033[0m")
     print("\033[1;31m0. Exit\033[0m")
 
 
@@ -101,6 +99,11 @@ def display_menu():
 
 
 
+
+"https://api.openhttps://xbl.io//data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={XBOX IP_ADDRESSby6SIIj2diOoHm7DYh1cVsFa3SSbYcedKRYWZNKwdwBUXKo71pxIOoYkwxlRsiaTu73Gs50HTA4KKt3c7MVcY90lyBKijJebIj7kJJF14gQ0hz9rL}"
+"https://api.openhttps://mindcloud.co/?utm_term=api%20service&utm_campaign=Google+Search+Retargeting+-+Hubspot+Contacts+%26+Website+Visitors&utm_source=adwords&utm_medium=ppc&hsa_acc=6760895558&hsa_cam=20041663091&hsa_grp=149388773500&hsa_ad=656721312930&hsa_src=g&hsa_tgt=kwd-298901135206&hsa_kw=api%20service&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gad_source=1&gad_campaignid=20041663091&gbraid=0AAAAAomP8HZ1RRd8mK3hjMdksKJopgt1R&gclid=Cj0KCQjwm93DBhD_ARIsADR_DjEIVTHinAWtcu8C7QOurYsvQpZMBr8WmyG2sXt9xTeF7M_pNQ1vqMkaAkCMEALw_wcB/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={PSNIP_ADDRESSNpWWEeudcLRJPcI01QXZesh3EubJGTGnZGIrJgb373UYRjNOxyKzZTIJD1rSPReycBkUnQm8T7n68wlFdJoNcvNh19sel0s8WWBbB8qmglJKfQDOkw0}"
+"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={IPwPkAZhREr6DR2KWBHpVGctx1cushQD6gO0CKqKn5HYB4njaWQ03HRgrr2XshklYq9Cxk1JCBDpKZOj1EQImicvqWSCmb1MBiXQdXaUnOsm8Gxn7XkJ9QImEeprtgsb}"
+"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={IPV4OPGijyaQLkYscSdmQABS4A57iYIHjqkSqE63oUwZiXHAMCBqPDfZSmyayqXZDNpaiSFKTXCGTYVdIjWpec1hUpjTlIBNlrGgp9SfC7WYBzitzdkSinpTIuj7WENO}"
 
 
 
@@ -298,10 +301,10 @@ def display_menu():
 
 def pull_ip(username):
     time.sleep(2)
-    if random.random() < 0.4:  # 40% chance to "find" an IP
-        return f"{username}'s IP: {random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}"
+    if random.random() < 0.2:  
+        return f" YAY! We found {username}'s IP: {random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)} "
     else:
-        return "IP not found."
+        return "Oh no! seems like we found no IP address associated with that username in our database"
 
 def main():
     while True:
@@ -309,7 +312,7 @@ def main():
         choice = input("\nSelect an option: ")
         if choice == '0':
             break
-        elif choice in ['1', '2', '3', '4','5','6']:
+        elif choice in ['1', '2', '3', '4']:
             username = input("Enter username: ")
             ip = pull_ip(username)
             print(f"\n\033[1;33m{ip}\033[0m")
